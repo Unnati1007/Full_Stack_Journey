a2c64904e3a22639868184df0689d5abad48c68d
Unnati1007 <unnatijadon1007@gmail.com>
2026-08-10 22:31:40 +0530

    feat: Add Auto-typed project using Typed.js

---
+++ js/JS DOM Projects/Auto-typed/script.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.auto-typed', {
+    strings: ['MERN Stack Developer', 'AI/ML Enthusiast', 'Python Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 1000,
+    cursorChar: "|",
+    smartBackspace: false,
+});
