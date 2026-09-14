65ea34ac2d4c58239c7084f5800be14da6b1ab51
Unnati1007 <unnatijadon1007@gmail.com>
2026-09-15 00:39:17 +0530

    feat(dom): add event flow capturing and bubbling example

---
+++ js/DOM Traversal/event-flow/script.js
@@ -0,0 +1,20 @@
+//add event listener to the div 
+document.getElementById("parentDiv").addEventListener("click", function (event) {
+    alert("Div clicked! Event Phase" + event.eventPhase);
+}, true); //capturing
+
+
+//add event listner to the ul 
+document.getElementById("list").addEventListener("click", function (event) {
+    alert("List clicked! Event Phase" + event.eventPhase);
+}, true); //capturing
+
+
+//add event listener to the li 
+document.getElementById("item1").addEventListener("click", function (event) {
+    alert("Item 1 clicked! Event Phase" + event.eventPhase);
+}, true); //babbling 
+
+document.getElementById("item2").addEventListener("click", function (event) {
+    alert("Item 2 clicked! Event Phase" + event.eventPhase);
+}, false); //bubbling 
